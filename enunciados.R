8fbb02242babf078e2a76200edb7c28718276c64
mario <ad.mcutillas@gmail.com>
2024-10-01 15:37:25 +0200

    2

---
+++ enunciados.R
@@ -0,0 +1,17 @@
+#PROBLEMA 1
+#Aplicar los criterios de decisión bajo incertidumbre al siguiente problema:
+#Tenemos tres posibles escenarios y tres posibles decisiones a tomar. La decisión 1
+#conlleva unos beneficios de 5, 3 y 9 unidades en cada uno de los escenarios. La decisión 2
+#conlleva unos beneficios de 0, 12 y 4 unidades en cada escenario. Por último, la decisión 
+#3 implica unas ganancias de 10, 1 y 6 unidades en cada escenario. 
+
+
+
+#PROBLEMA 2
+#Una fondo de inversiones estudia realizar una inversión en uno de entre 5 productos
+#financieros distintos. Dependiendo de el comportamiento de la bolsa 
+#a nivel global, los rendimientos esperados para las inversiones (i1, i2, i3, i4, i5) son:
+#Para la inversión 1: 0, 12 ,3, 20 (millones de euros)
+#Para la inversion 2: 5, 5, 5, 5 (millones de euros)
+#Para la inversión 3: 4, 9, 10, 7 (millones de euros)
+#
